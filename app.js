e90b9e16087985a9a4b03fab6925abf3febb0006
Livia_Santos <liviamelq@gmail.com>
2018-03-29 15:08:03 +1100

    implement add task fucntion

---
+++ app.js
@@ -0,0 +1,52 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// load all event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+}
+
+// Add Task
+function addTask(e) {
+  if (taskInput.value === '') {
+    alert('Add a task');
+  }
+  // create li element
+  const li = document.createElement('li');
+  // add a class
+  li.className = 'collection-item';
+  // create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // create a new link element
+  const link = document.createElement('a');
+  // add class
+  link.className = 'delete-item secondary-content';
+  // add the icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // append the link to the li
+  li.appendChild(link);
+  // append the li to the ul
+  taskList.appendChild(li);
+  // Clear input
+  taskInput.value = '';
+
+  e.preventDefault();
+}
+
+
+
+
+
+
+
+
+
+
+//
