6600eb9014fe496490fdf0d8cdfa6db7b5686baf
Livia_Santos <liviamelq@gmail.com>
2018-03-29 18:01:04 +1100

    implement remove task

--- app.js
+++ app.js
@@ -11,33 +11,44 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // remove task event
+  taskList.addEventListener('click', removeTask);
 }
 
 // Add Task
 function addTask(e) {
   if (taskInput.value === '') {
     alert('Add a task');
+  } else {
+    // create li element
+    const li = document.createElement('li');
+    // add a class
+    li.className = 'collection-item';
+    // create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // create a new link element
+    const link = document.createElement('a');
+    // add class
+    link.className = 'delete-item secondary-content';
+    // add the icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // append the link to the li
+    li.appendChild(link);
+    // append the li to the ul
+    taskList.appendChild(li);
+    // Clear input
+    taskInput.value = '';
   }
-  // create li element
-  const li = document.createElement('li');
-  // add a class
-  li.className = 'collection-item';
-  // create text node and append to li
-  li.appendChild(document.createTextNode(taskInput.value));
-  // create a new link element
-  const link = document.createElement('a');
-  // add class
-  link.className = 'delete-item secondary-content';
-  // add the icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  // append the link to the li
-  li.appendChild(link);
-  // append the li to the ul
-  taskList.appendChild(li);
-  // Clear input
-  taskInput.value = '';
-
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
 }
 
 
